6651bb57615225a6b55da69d90c8284bf837ceb3
shamaara <162900659+shamaara@users.noreply.github.com>
2024-03-10 16:44:00 +0000

    done

---
+++ shama/shame2.js
@@ -0,0 +1,24 @@
+const accountId = 123456789;
+let accountEmail = "mdm@google.com";
+var accountPassward = "789456123";
+accountCity = "araria";
+let accountState = "biahar";
+let accountHobby = "Dance";
+
+
+
+// accountId = 987654321;
+
+// note :- prefer not to use var 
+// because of issue in block scope and functional scope problem
+
+accountEmail = "shama@google.com";
+accountPassward = "hellodear";
+accountCity = "patna";
+accountState = "Delhi";
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassward,accountCity,accountState,accountHobby]);
+
