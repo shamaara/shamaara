e6752727fb4ddc17afcf762aeb7e679085d2002a
shamaara <162900659+shamaara@users.noreply.github.com>
2024-03-10 17:08:17 +0000

    Done

--- shama/sha1.js
+++ shama/sha1.js
@@ -1,1 +1,20 @@
-console.log("Shamaara")
+const accountId = 999999999;
+let accoountFirstname = "md";
+let accountLastname = "jivan";
+let accountMothername = "kajal";
+let accountFathername = "Ravi raja";
+let accoountVillage = "sandalpur";
+let accoountPost = "ahilgown";
+let accountCity = "araria";
+let accountState = "Bihar";
+let accountCountry = "India";
+let accountEmail = "mdm9988@gmail.com";
+let accountPhone = "9988776655";
+let accountGender = "Male";
+let accountMaritalstatus = "single";
+let accountHobby = "Dance";
+
+
+console.log(accountId);
+
+console.table([accoountFirstname, accountLastname, accountMothername, accountFathername, accoountVillage, accoountPost, accountCity, accountEmail,  accountPhone, accountGender, accountMaritalstatus, accountHobby]);
